a2f3ccdd4c0d4a08d166f84df11fdd8bde5544ee
Thainá Santos <santosoliveirathaina80@gmail.com>
2023-09-12 16:12:26 -0300

    commit da lista de tarefas

---
+++ js/array.js
@@ -0,0 +1,29 @@
+                                                // forma literal //
+//array sem elementos
+let lista = [];
+
+//array com elementos numéricos
+let numeros = [1,3,5,7,9,11,13];
+
+//numeros (7) [1, 3, 5, 7, 9, 11, 13]
+
+
+//array com booleano, número e string
+let misc = [false, 6.5, "thainá"];
+undefined
+//misc (3) [false, 6.5, 'thainá']
+
+
+ undefined //confirmação 
+
+
+
+                                            // construtora array//
+ //chamada sem argumentos - é equivalente ao array literal
+ var a = new Array();
+
+ //chamada com um unico argumento numerico especifica um comprimento
+ var b = new Array(10)
+
+ //elementos de array ou de apenas um elemento não numérico para o array
+ var c = new Array(5, 4, 3, 2, 1, "testing, testing");
